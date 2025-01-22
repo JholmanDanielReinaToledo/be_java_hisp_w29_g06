0dd6d7d7607bb2e5905c2750bd1fb1cb5c4d4b10
dbayona_meli <diego.bayona@mercadolibre.com.co>
2025-01-22 15:27:47 -0500

    delete test code

--- src/main/java/com/meli/socialmeli/repository/PostRepositoryImpl.java
+++ src/main/java/com/meli/socialmeli/repository/PostRepositoryImpl.java
@@ -1,14 +1,11 @@
 package com.meli.socialmeli.repository;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
 import com.meli.socialmeli.entity.Post;
-import com.meli.socialmeli.entity.Product;
-import com.meli.socialmeli.entity.Seller;
 
 @Repository
 public class PostRepositoryImpl implements IPostRepository {
@@ -16,42 +13,7 @@ public class PostRepositoryImpl implements IPostRepository {
 
     public PostRepositoryImpl() {
         this.posts = new ArrayList<Post>();
-        // Initialize posts with sample data
-        Seller sample_seller = new Seller(1, "seller1");
-        Seller sample_seller2 = new Seller(2,"seller2");
-        // Add more posts as needed
-        this.posts.add(new Post(1,
-                                LocalDate.now(),
-                                20.0,
-                                new Product(),
-                                sample_seller,
-                                0.20,
-                                true,
-                                2));
-        this.posts.add(new Post(2,
-                                LocalDate.now(),
-                                30.0,
-                                new Product(),
-                                sample_seller2,
-                                0.15,
-                                false,
-                                1));
-        this.posts.add(new Post(3,
-                                LocalDate.now(),
-                                40.0,
-                                new Product(),
-                                sample_seller,
-                                0.10,
-                                true,
-                                3));
-        this.posts.add(new Post(4,
-                                LocalDate.now(),
-                                50.0,
-                                new Product(),
-                                sample_seller2,
-                                0.05,
-                                false,
-                                4));
+        
 
     }
     
