7a63f43f63f155822302576aba374699a90ea46c
dyhurtado_meli <dylan.hurtado@mercadolibre.com>
2025-01-22 16:49:12 -0300

    Historia de usuario 2

--- src/main/java/com/meli/socialmeli/entity/Seller.java
+++ src/main/java/com/meli/socialmeli/entity/Seller.java
@@ -4,11 +4,13 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class Seller extends User {
     private List<User> followers = new ArrayList<>();
