36860673cb4d3c4fac8c76613a8a7f584f1e7693
dyhurtado_meli <dylan.hurtado@mercadolibre.com>
2025-01-23 12:27:16 -0300

    Historia de usuario 2

--- src/main/java/com/meli/socialmeli/service/IUserService.java
+++ src/main/java/com/meli/socialmeli/service/IUserService.java
@@ -1,9 +1,6 @@
 package com.meli.socialmeli.service;
 
-import com.meli.socialmeli.dto.FollowedDto;
 import com.meli.socialmeli.dto.ResponseDto;
-
-import java.util.List;
 
 public interface IUserService {
 
--- src/main/java/com/meli/socialmeli/service/UserServiceImpl.java
+++ src/main/java/com/meli/socialmeli/service/UserServiceImpl.java
@@ -1,16 +1,13 @@
 package com.meli.socialmeli.service;
 
-import com.meli.socialmeli.dto.FollowedDto;
 import com.meli.socialmeli.dto.ResponseDto;
 import com.meli.socialmeli.entity.Seller;
 import com.meli.socialmeli.entity.User;
 import com.meli.socialmeli.exception.NotFoundException;
 import com.meli.socialmeli.repository.ISellerRepository;
 import com.meli.socialmeli.repository.IUserRepository;
-import com.meli.socialmeli.repository.SellerRepositoryImpl;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
