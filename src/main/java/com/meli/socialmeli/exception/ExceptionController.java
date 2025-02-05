1ad76083201a548639f8d4c2758f2d8fa4ad4848
JholmanDanielReinaToledo <reina.daniel1005@gmail.com>
2025-02-05 09:56:33 -0500

    adding exception manage errors in advice contrllers

--- src/main/java/com/meli/socialmeli/exception/ExceptionController.java
+++ src/main/java/com/meli/socialmeli/exception/ExceptionController.java
@@ -1,10 +1,16 @@
 package com.meli.socialmeli.exception;
 
 import com.meli.socialmeli.dto.ExceptionDto;
+import com.meli.socialmeli.dto.response.ResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class ExceptionController {
@@ -37,6 +43,19 @@ public class ExceptionController {
     public ResponseEntity<ExceptionDto> noSellersFollowed(NoSellersFollowedException e){
         ExceptionDto exceptionDto = new ExceptionDto(e.getMessage());
         return new ResponseEntity<>(exceptionDto, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    protected ResponseEntity<ResponseDto> handleValidationExceptions(HttpMessageNotReadableException e) {
+        ResponseDto error = new ResponseDto(e.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
 }
